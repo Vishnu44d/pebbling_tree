7cd7aef0d62d97b58af0e86c070ca02b850ee9f5
Vishnu Deo Gupta <vishnu44d@gmail.com>
2018-11-18 01:31:29 +0530

    function to convert adjency list to pointer type tree is added

--- runner.py
+++ runner.py
@@ -1,24 +1,25 @@
 from peb.tree.tree import Tree
-
-tree = Tree()
-
-tree.add_node("0")  # root node
-tree.add_node("1", "0")
-tree.add_node("2", "0")
-tree.add_node("3", "1")
-tree.add_node("4", "1")
-tree.add_node("5", "2")
-tree.add_node("6", "2")
-tree.add_node("7", "6")
-tree.add_node("8", "6")
-tree.add_node("9", "5")
-tree.add_node("10", "5")
-tree.display("0")
+from peb.test_graph import adj_list, unweighted_graph
 
 
 
-'''from peb.test_graph import adj_list
-from peb.visible_edge import find_visible_edges
 
-print(adj_list)'''
-#print(find_visible_edges(adj_list))
+
+
+def adj_to_tree(graph):
+    tree = Tree()
+    for vertex, i in zip(graph, range(len(graph))):
+        if i == 0:
+            tree.add_node(vertex)
+        else:
+            tree.add_node(vertex, graph[vertex][0])
+    return tree
+
+
+
+if __name__ == "__main__":
+    graph = unweighted_graph(adj_list)
+    adj_to_tree(graph).display("A")
+
+
+
